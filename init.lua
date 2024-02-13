f87b1aaeed9a01ed854cbdf238179fdb1fbd16af
Annie Hu <anniegracehu@gmail.com>
2024-02-12 19:54:27 -0600

    Delete init.lua

--- init.lua
+++
@@ -1,7 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require("config.options")
-vim.opt["expandtab"] = true
-vim.opt["tabstop"] = 4
-vim.opt["shiftwidth"] = 4
-vim.opt["softtabstop"] = 4
