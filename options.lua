df03f5d2d37bee0fc8a4e3c1b999eff5000d48cd
Annie Hu <anniegracehu@gmail.com>
2024-02-12 19:53:28 -0600

    Add files via upload

---
+++ options.lua
@@ -0,0 +1,8 @@
+vim.opt["expandtab"] = true
+vim.opt["tabstop"] = 8
+vim.opt["shiftwidth"] = 8
+vim.opt["softtabstop"] = 8
+vim.opt.expandtab = true
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.softtabstop = 8
